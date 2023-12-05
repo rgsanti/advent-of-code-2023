86d2aa0778b1c6a21f614dbb1abd7e37a7638c01
Raphael Santiago <raphsanti@Raphaels-MacBook-Pro.local>
2023-12-05 19:16:04 +0000

    complete day 4

--- 04/2/solution.py
+++ 04/2/solution.py
@@ -25,38 +25,14 @@ for string in lines:
     num_matches = split_card(string)
     matches.append(num_matches)
 
-# f = open('test.txt')
-# data = f.read()
-# lines = data.splitlines()
-
-# matches = []
-# for string in lines:
-#     num_matches = split_card(string)
-#     matches.append(num_matches)
-
-# print(matches)
-
-# def resolve_card(index, num_matches):
-#     print(f'Resolving Card {index + 1} (Matches = {num_matches})')
-#     total = 1
-#     print(f'Loop from {index + 1} to {index + 1 + num_matches}')
-#     for i in range(index + 1, index + 1 + num_matches):
-#         print(f'Resolving Card {index + 1}\'s copy of Card {i + 1}')
-#         total += resolve_card(i, matches[i])
-#     return total
-
 def resolve_card(index, num_matches):
-    print(f'Resolving Card {index + 1} (Matches = {num_matches})')
     total = 1
-    print(f'Loop from {index + 1} to {index + 1 + num_matches}')
     for i in range(index + 1, index + 1 + num_matches):
-        print(f'Resolving Card {index + 1}\'s copy of Card {i + 1}')
         total += resolve_card(i, matches[i])
     return total
 
 total_cards = 0
-
 for index, num_matches in enumerate(matches):
     total_cards += resolve_card(index, num_matches)
 
-print(total_cards)
+print(f'Answer: {total_cards}')
